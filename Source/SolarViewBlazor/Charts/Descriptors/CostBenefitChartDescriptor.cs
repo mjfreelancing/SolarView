bb91c19dfec630b7f5a3c9edf6e13d57a4c61da2
mjfreelancing <malcolm@mjfreelancing.com>
2020-07-09 18:58:48 +1000

    Split the existing chart into two

---
+++ Source/SolarViewBlazor/Charts/Descriptors/CostBenefitChartDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using SolarViewBlazor.Components.Charts;
+using System;
+using System.Collections.Generic;
+
+namespace SolarViewBlazor.Charts.Descriptors
+{
+  public class CostBenefitChartDescriptor : IChartDescriptor
+  {
+    public string Id => nameof(CostBenefitChartDescriptor);
+    public string Description => "Cost Benefit";
+    public Type ChartType => typeof(CostBenefitChart);
+    public IDictionary<string, object> Parameters { get; } = new Dictionary<string, object>();
+  }
+}
